44f117f36c6d22ff280c5c2e6772f7b243aa6dac
Manav361 <manavagrawal100@gmail.com>
2023-02-27 15:35:47 +0530

    Class Based API added

--- sds_backend/views.py
+++ sds_backend/views.py
@@ -1,5 +1,4 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from base.models import SlideQue
 from rest_framework.views import APIView
 from .serializers import SlideQueSerializer
