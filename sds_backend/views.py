0d0f074dd6ba8baad7afe0c11bc921a3c9ea2131
Manav361 <manavagrawal100@gmail.com>
2023-02-27 13:07:11 +0530

    views.py

--- sds_backend/views.py
+++ sds_backend/views.py
@@ -1,17 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import SlideQue
+from rest_framework.views import APIView
 from .serializers import SlideQueSerializer
 
-@api_view(['GET'])
-def getData(request):
-    a=SlideQue.objects.all()
-    serializer = SlideQueSerializer(a, many=True)
-    return Response(serializer.data)
+class SlideQueView(APIView):
+    def get(self,request,format=None):
+        a=SlideQue.objects.all()
+        serializer = SlideQueSerializer(a, many=True)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def addData(request):
-    serializer = SlideQueSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    def post(self,request,format=None):
+        serializer = SlideQueSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    
