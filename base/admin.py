93cc7dfa255403995bde27f5edd2a12a32ee6ec9
Ishan <ishangrover13404@gmail.com>
2023-03-01 13:45:09 +0530

    Added FileField to model SlideQue

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import SlideQue
 # Register your models here.
+admin.site.register(SlideQue)
--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class SlideQue(models.Model):
     SlideId = models.CharField(max_length=10)
+    Slide=models.FileField(upload_to="slides",blank=True)
     PageNo = models.IntegerField()
     Question = models.CharField(max_length=100)
     Answer = models.CharField(max_length=500)
